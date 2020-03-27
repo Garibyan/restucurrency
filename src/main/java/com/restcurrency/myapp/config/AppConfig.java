607dfc1a96b2ef5f14210ef3827ec89a9033d0c9
Armen <garibyan923@gmail.com>
2020-03-27 18:38:32 +0400

    with intercaptor

--- src/main/java/com/restcurrency/myapp/config/AppConfig.java
+++ src/main/java/com/restcurrency/myapp/config/AppConfig.java
@@ -1,13 +1,39 @@
 package com.restcurrency.myapp.config;
 
+import com.restcurrency.myapp.intercaptor.LoggingInterceptor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 @Configuration
-public class AppConfig {
+@ComponentScan("com.restcurrency")
+@EnableWebMvc
+public class AppConfig extends WebMvcConfigurerAdapter  {
+
+    @Bean
+    public UrlBasedViewResolver setupViewResolver() {
+        UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+        resolver.setPrefix("/views/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+        return resolver;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggingInterceptor());
+    }
+
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
 }
+
+
