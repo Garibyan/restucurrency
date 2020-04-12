cdf554f5d44431de84c81b2953211fe316dd0449
Armen <garibyan923@gmail.com>
2020-04-12 17:04:33 +0400

    resttemplate intercaptor

--- src/main/java/com/restcurrency/myapp/intercaptor/RestTemplateHeaderModifierInterceptor.java
+++ src/main/java/com/restcurrency/myapp/intercaptor/RestTemplateHeaderModifierInterceptor.java
@@ -6,6 +6,8 @@ import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class RestTemplateHeaderModifierInterceptor implements ClientHttpRequestInterceptor {
 
@@ -16,8 +18,20 @@ public class RestTemplateHeaderModifierInterceptor implements ClientHttpRequestI
             ClientHttpRequestExecution execution) throws IOException {
 
         ClientHttpResponse response = execution.execute(request, body);
-        response.getHeaders().add("Foo", "bar");
-        System.out.println("request was intercapted");
+//        response.getHeaders().add("Foo", "bar");
+        System.out.println("Request was intercapted");
+        System.out.println("Method: " + request.getMethod());
+        System.out.println("Url: " + request.getURI());
+        System.out.println("Methodvalue: " + request.getMethodValue());
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd/yyyy");
+        System.out.println("Date: " + simpleDateFormat.format(new Date()));
+        System.out.println("---Request Completed---");
+        System.out.println("=========================");
+        System.out.println("Response body: " + response.getBody());
+        System.out.println("Responce headers: " + response.getHeaders());
+        System.out.println("Responce statustext: " + response.getStatusText());
+        System.out.println("Responce class: " + response.getClass());
+        System.out.println("Responce raw status code: " + response.getRawStatusCode());
         return response;
     }
 }
